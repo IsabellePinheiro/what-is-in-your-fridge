75ede7bef5ad8c4c70140aca48c833658abfdf0d
IsabellePinheiro <isabellepinheiro00@gmail.com>
2022-12-21 12:59:36 -0300

    chore: remove unused code

--- src/components/searchBar/index.tsx
+++ src/components/searchBar/index.tsx
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 
-interface ISearchBar{
-  placeholder: string
+interface SearchBarProps {
+  placeholder: string;
 }
 
-export default function SearchBar({placeholder}:ISearchBar ){
-  const [value, setValue] = useState('')
-  
+export default function SearchBar({ placeholder }: SearchBarProps) {
+  const [value, setValue] = useState("");
+
   return (
-       <input
-        className='flex w-full mt-4 px-6 py-4 rounded bg-gray-300 border border-gray-600 text-sm text-gray-900'
-        type='text'  value={value} onChange={(event) => { setValue(event.target.value)}}  placeholder={placeholder}/>
- 
-  )
+    <input
+      className="flex w-full mt-4 px-6 py-4 rounded bg-gray-300 border border-gray-600 text-sm text-gray-900"
+      type="text"
+      value={value}
+      onChange={(event) => {
+        setValue(event.target.value);
+      }}
+      placeholder={placeholder}
+    />
+  );
 }
