81fab42576e6070b78c95c82d37892b625830594
IsabellePinheiro <isabellepinheiro00@gmail.com>
2022-12-22 13:03:06 -0300

    feat: add memoized function

--- src/components/searchBar/index.tsx
+++ src/components/searchBar/index.tsx
@@ -1,16 +1,22 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 
 export default function SearchBar() {
   const [value, setValue] = useState("");
+
+  const handleChangeInput = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      event.preventDefault();
+      setValue(event.target.value);
+    },
+    []
+  );
 
   return (
     <input
       className="flex w-full mt-4 px-6 py-4 rounded bg-gray-300 border border-gray-600 text-sm text-gray-900"
       type="text"
       value={value}
-      onChange={(event) => {
-        setValue(event.target.value);
-      }}
+      onChange={handleChangeInput}
       placeholder="TYPE YOUR INGREDIENTS HERE"
     />
   );
