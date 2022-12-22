4de2a71ef6a56c4553054051c870cd54d0fddaef
IsabellePinheiro <isabellepinheiro00@gmail.com>
2022-12-22 12:12:53 -0300

    style: run prettier

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children?: React.ReactNode
+  children?: React.ReactNode;
 }
 
-export default function Button({ children,  ...props }: ButtonProps) {
+export default function Button({ children, ...props }: ButtonProps) {
   return (
     <button
       className="bg-gray-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700"
