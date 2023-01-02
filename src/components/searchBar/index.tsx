f3b8039292a5c84c1a5c0594cfa45532fe21284b
IsabellePinheiro <isabellepinheiro00@gmail.com>
2023-01-02 13:40:58 -0300

    fix: prevent user to select same ingredient

--- src/components/searchBar/index.tsx
+++ src/components/searchBar/index.tsx
@@ -1,65 +1,75 @@
 import React, { useCallback, useState } from "react";
 
-interface SearchResultListProps {
-  id: number;
+interface IngredientProps {
+  id: string;
   name: string;
 }
 
 export default function SearchBar() {
-  const [value, setValue] = useState("");
-  const [searchResultList, setSearchResultList] = useState<SearchResultListProps[]>([]);
-  const [selectedIngredientsList, setSelectedIngredientsList] = useState<SearchResultListProps[]>([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [ingredients, setIngredients] = useState<IngredientProps[]>([]);
+  const [selectedIngredients, setSelectedIngredients] = useState<IngredientProps[]>([]);
 
   const handleChangeInput = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
       event.preventDefault();
-      setValue(event.target.value);
+      setSearchQuery(event.target.value);
       fetch(
         `https://api.spoonacular.com/food/ingredients/autocomplete?apiKey=${process.env.NEXT_PUBLIC_SPOONACULAR_API_KEY}&query=${event.target.value}&number=10`
       )
         .then((res) => res.json())
         .then((result) => {
-          setSearchResultList(result);
+          setIngredients(result);
         });
     },
     []
   );
 
+  const generateKey = (pre: IngredientProps) =>  `${ pre.name.trim() }_${ new Date().valueOf() }`;
+
+
   const handleSelectIngredient = useCallback(
-    (ingredient: SearchResultListProps) => {
-      setSelectedIngredientsList((current) => [
+    (ingredient: IngredientProps) => {
+      if(selectedIngredients.some((selected) => selected.name === ingredient.name)){
+        alert('Ingredient already selected. Please, choose another one.')
+        return;
+      }
+      setSelectedIngredients((current) => [
         ...current,
         {
           ...ingredient,
-          id: new Date().getTime(),
+          id: generateKey(ingredient),
         },
       ]);
     },
-    []
-  );
+    [selectedIngredients]
+    );
 
-  const handleDeleteIngredient = useCallback((index: number) => {
-    setSelectedIngredientsList((prevState) => prevState.filter((item) => item.id !== index));
+
+  const handleDeleteIngredient = useCallback((index: string) => {
+    setSelectedIngredients((prevState) => prevState.filter((item) => item.id !== index));
   }, []);
+
+
 
   return (
     <>
       <input
         className="flex w-full mt-4 px-6 py-4 rounded bg-gray-300 border border-gray-600 text-sm text-gray-900"
         type="text"
-        value={value}
+        value={searchQuery}
         onChange={handleChangeInput}
         placeholder="TYPE YOUR INGREDIENTS HERE"
       />
-      {searchResultList && (
-        <ul>
-          {searchResultList.map((item, index) => (
+      {ingredients.length > 0 && (
+        <ul> 
+          {ingredients.map((item) => (
             <li
-              key={index}
+              key={ generateKey(item) + Math.random()}
               className="bg-teal-100 h-8 cursor-pointer"
               onClick={() => handleSelectIngredient(item)}
             >
-              <strong className="px-2 py-4  text-teal-600 hover:text-teal-900">
+              <strong className="px-2 py-4 text-teal-600 hover:text-teal-900">
                 {item.name}
               </strong>
             </li>
@@ -67,9 +77,9 @@ export default function SearchBar() {
         </ul>
       )}
       <div className="flex flex-row my-3">
-        {selectedIngredientsList.map((ingredient, index) => (
+        {selectedIngredients.map((ingredient) => (
           <div
-            key={index}
+            key={ingredient.id}
             className="bg-green-100 w-fit border border-gray-600 p-1 mr-3"
           >
             <span>{ingredient.name}</span>
