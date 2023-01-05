f0450212766243f8c9ea322d85e61220c18fa9a5
IsabellePinheiro <isabellepinheiro00@gmail.com>
2023-01-05 14:20:17 -0300

    feat: display recipes results

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAtom } from "jotai";
 import Button from "../components/button";
 import Header from "../components/header";
-import SearchBar from "../components/searchBar";
+import SearchBar, { searchAtom } from "../components/searchBar";
+import * as api from "../services/api";
+import Recipe from "../components/recipe";
+
+interface RecipeResult {
+  id: number;
+  title: string;
+  image: string;
+  cuisines: string[];
+  summary: string;
+}
 
 export default function Home() {
+  const [search] = useAtom(searchAtom);
+  const [recipes, setRecipes] = useState<RecipeResult[]>([]);
+
+  async function handleSearchRecipes() {
+    if (search.length) {
+      const formattedIngredientsUrl = search
+        .map((ingredient, index) => `${index > 0 ? "+" : ""}${ingredient.name}`)
+        .join(`,`);
+
+      const response = await api.searchRecipesByIngredients(
+        formattedIngredientsUrl
+      );
+      setRecipes(response);
+      return;
+    }
+    try {
+      const response = await api.getRandomRecipes();
+      setRecipes(response);
+    } catch (error) {
+      alert((error as Error).message);
+    }
+  }
+
   return (
     <div className="px-8 ">
       <Header />
       <SearchBar />
-      <Button>SEARCH</Button>
+      <Button onClick={() => handleSearchRecipes()}>SEARCH</Button>
+
+      <div className="grid gap-x-8 gap-y-4 grid-cols-3">
+        {recipes?.map((recipe) => (
+          <Recipe
+            key={recipe.id}
+            title={recipe.title}
+            image={recipe.image}
+            cuisines={recipe.cuisines}
+            summary={recipe.summary}
+          />
+        ))}
+      </div>
     </div>
   );
 }
